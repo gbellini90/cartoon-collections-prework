3e479c67dc87b5f58456543905ad0f424c0b852b
Gabrielle Bellini <gbellini90@gmail.com>
2018-10-08 18:58:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,9 +19,9 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |item|
-   cheese_types.include? |item|
+   cheese_types.include?(item)
   end
 end
  
  
-   
+ 
